bcfe20e1fb99ea257303d407102e09d2599ace55
Scott Davis <me@sdavis.info>
2016-08-03 23:38:32 -0400

    add libsass to default mix

--- lib/mix/tasks/compile.sass.ex
+++ lib/mix/tasks/compile.sass.ex
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.Compile.Sass do
 
   @shortdoc "Compiles libsass library"
   def run(_) do
-    if Mix.shell.cmd("make priv/sass_nif.so") != 0 do
+    if Mix.shell.cmd("make") != 0 do
       raise Mix.Error, message: "could not run `make priv/sass.so`. Do you have make and gcc installed?"
     end
   end
