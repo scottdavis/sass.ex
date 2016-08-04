afeb7aac40f35b0796afc1de520a74bd8a74ee4a
Scott Davis <me@sdavis.info>
2016-08-03 20:13:16 -0400

    cleanup

--- src/sass_nif.c
+++ src/sass_nif.c
@@ -25,27 +25,6 @@ static inline ERL_NIF_TERM make_tuple(ErlNifEnv* env, const char* mesg, const ch
   return enif_make_tuple2(env, atom, str);
 }
 
-
-char* utf8cpy(char* dst, const char* src, size_t sizeDest )
-{
-	if( sizeDest ){
-		size_t sizeSrc = strlen(src); // number of bytes not including null
-		while( sizeSrc >= sizeDest ){
-
-			const char* lastByte = src + sizeSrc; // Initially, pointing to the null terminator.
-			while( lastByte-- > src )
-				if((*lastByte & 0xC0) != 0x80) // Found the initial byte of the (potentially) multi-byte character (or found null).
-					break;
-
-			sizeSrc = lastByte - src;
-		}
-		memcpy(dst, src, sizeSrc);
-		dst[sizeSrc] = '\0';
-	}
-	return dst;
-}
-
-
 static int my_enif_list_size(ErlNifEnv* env, ERL_NIF_TERM list)
 {
   ERL_NIF_TERM head, tail, nexttail;
