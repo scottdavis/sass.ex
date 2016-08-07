82457fc4b0363357987f53094ec66188836f2e13
Scott Davis <me@sdavis.info>
2016-08-06 22:26:45 -0400

    checkout libsass before make

--- mix.exs
+++ mix.exs
@@ -6,13 +6,12 @@ defmodule Sass.Mixfile do
   def project do
     [
       app:         :sass,
-      version:     "1.0.2",
+      version:     "1.0.3",
       elixir:      "~> 1.3.0",
       compilers:   [:sass, :elixir, :app],
       deps:        deps(),
       package:     package,
       description: description,
-      compilers: [:elixir_make] ++ Mix.compilers,
       make_clean: ["clean"],
       docs: [logo: "assets/sass.png",
           extras: ["README.md"]],
@@ -40,24 +39,14 @@ defmodule Sass.Mixfile do
         GitHub: "https://github.com/scottdavis/sass.ex",
         Issues: "https://github.com/scottdavis/sass.ex/issues",
         Source: "https://github.com/scottdavis/sass.ex"
-      },
-      files: [
-        "lib",
-        "src",
-        "libsass_src",
-        "priv",
-        "Makefile",
-        "mix.exs",
-        "README.md",
-        "LICENSE"
-      ]
+      }
     ]
   end
 
   defp deps do
     [
-      {:libsass, github: "sass/libsass", tag: "3.3.6", app: false},
-      {:markdown, github: "devinus/markdown"},
+      {:libsass, github: "sass/libsass", tag: "3.3.6", only: :prod, app: false, optional: false},
+      {:markdown, github: "devinus/markdown", only: :docs},
       {:ex_doc, "0.13.0", only: :docs},
       {:inch_ex, "~> 0.2", only: :docs},
     ]
