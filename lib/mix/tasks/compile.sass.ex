5622a3271e444ef74eb37638890b3c654afd674e
Scott Davis <me@sdavis.info>
2016-08-07 21:50:37 -0400

    use https for git

--- lib/mix/tasks/compile.sass.ex
+++ lib/mix/tasks/compile.sass.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.Compile.Sass do
   def run(_) do
     dir = "#{__DIR__}../../deps/libsass"
     if File.dir?(dir) do
-      Mix.shell.cmd("git clone --branch 3.3.6 git@github.com:sass/libsass.git #{dir}")
+      Mix.shell.cmd("git clone --branch 3.3.6 https://github.com/sass/libsass.git #{dir}")
     end
     if Mix.shell.cmd("make") != 0 do
       raise Mix.Error, message: "could not run `make priv/sass.so`. Do you have make and gcc installed?"
